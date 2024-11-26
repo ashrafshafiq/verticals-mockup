5faee181d9a982b5dcbbf4cd48d500fc4dbcb089
Ashraf Ullah <{ID}+{username}@users.noreply.github.com>
2024-11-26 18:03:28 -0500

    fixed roiute

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; 
 import Layout from "./components/Layout";
-import Tracking from "./pages/Tracking"; 
+import Tracking from "./pages/Tracking";
 import Donate from "./pages/Donate";
 import Campaigns from "./pages/Campaigns";
-import TrackingSearch from "./pages/TrackingSearch"; 
-import TrackingID from "./pages/TrackingID"; // Add this back if it's the component for "/tracking-id/assigned"
+import TrackingSearch from "./pages/TrackingSearch";
+import TrackingID from "./pages/TrackingID";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/verticals-mockup"> {/* Set the base name */}
       <Layout>
         <Routes>
-          <Route path="/" element={<Navigate to="/campaigns" />} />
+          <Route path="/" element={<Navigate to="/campaigns" />} /> {/* Redirect home to Campaigns */}
           <Route path="/campaigns" element={<Campaigns />} />
           <Route path="/donate/:id" element={<Donate />} />
           <Route path="/tracking" element={<Tracking />} />
           <Route path="/tracking-search" element={<TrackingSearch />} />
-          <Route path="/tracking-id/assigned" element={<TrackingID />} /> {/* Add this */}
+          <Route path="/tracking-id/assigned" element={<TrackingID />} />
         </Routes>
       </Layout>
     </Router>
